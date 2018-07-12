b8912ca51c581ac0abd74cd07465bb18ed934241
amandademetrio <amandademetrio@gmail.com>
2018-07-12 15:14:29 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,172 @@
+from flask import Flask, render_template, redirect, request, session, flash
+from mysqlconnection import connectToMySQL
+from flask_bcrypt import Bcrypt
+import datetime
+import re
+
+app = Flask(__name__)
+app.secret_key = 'Alfie'
+mysql = connectToMySQL('wall_users')
+now = datetime.datetime.now()
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def renderIndex():
+    if 'logged_in' in session and 'user_id' in session and session['logged_in'] == True:
+        #Getting the user_id
+        data = {
+                'user_id': session['user_id'],
+            }
+        #Getting the amount of messages received by the user
+        n_received_query = "SELECT COUNT(message) FROM messages WHERE receiver_id LIKE %(user_id)s"
+        n_received_messages = mysql.query_db(n_received_query,data)
+        session['n_received_messages'] = n_received_messages[0]['COUNT(message)']
+
+        #Getting actual messages received
+        received_query = "SELECT messages.id,messages.message,messages.created_at,regusers.first_name as sender_name FROM messages JOIN regusers ON messages.sender_id=regusers.id WHERE receiver_id LIKE %(user_id)s;"
+        received_messages = mysql.query_db(received_query,data)
+
+        #Getting the amount of sent messages
+        sent_query = "SELECT COUNT(message) FROM messages WHERE sender_id LIKE %(user_id)s"
+        n_sent_messages = mysql.query_db(sent_query,data)
+        session['n_sent_messages'] = n_sent_messages[0]['COUNT(message)']
+
+        #Getting all other users
+        all_users_query = "SELECT id,first_name FROM regusers WHERE id NOT LIKE %(user_id)s;"
+        all_users = mysql.query_db(all_users_query,data)
+
+        return render_template("index.html",received_messages=received_messages,all_users=all_users)
+    else:
+        return render_template("login_and_registration.html")
+
+@app.route('/process_registration',methods=['POST'])
+def procRegistration():
+    #Getting current emails in the database,for comparison
+    current_emails = mysql.query_db("SELECT email FROM regusers")
+
+    #first_name should be longer than 2 characters and all letters:
+    if len(request.form['first_name']) < 2:
+        flash("First Name must be at least 2 characters","name_error")
+    elif not request.form['first_name'].isalpha():
+        flash("First Name must have only letters","name_error")
+    else:
+        session['first_name'] = request.form['first_name']
+    #last_name should be longer than 2 characters and all letters
+    if len(request.form['last_name']) < 2:
+        flash("Last Name must be at least 2 characters","lname_error")
+    elif not request.form['last_name'].isalpha():
+        flash("Last Name must be letters only","lname_error")
+    else:
+        session['last_name'] = request.form['last_name']
+    #email address must be new and valid format
+    for item in current_emails:
+        if request.form['email'] == item['email']:
+            flash("Email already in list!","email_error")
+    if not EMAIL_REGEX.match(request.form['email']):
+        flash("Invalid Email Address!", "email_error")
+    else:
+        session['email'] = request.form['email']
+    #Password must be at least 8 characters; must match confirm password
+    if len(request.form['password']) < 8:
+        flash("Password must have at least 8 characters","password_error")
+    elif request.form['confirm_password'] != request.form['password']:
+        flash("Confirm password must match original password","cpassword_error")
+
+    if '_flashes' in session.keys():
+        return redirect("/")
+    else:
+        #Hashing password and adding it to session
+        session['password'] = bcrypt.generate_password_hash(request.form['password'])
+        session['logged_in'] = True
+        data = {
+                'first_name': session['first_name'],
+                'last_name': session['last_name'],
+                'email': session['email'],
+                'password': session['password'],
+                'created_at': now,
+                'updated_at': now
+            }
+        query = "INSERT INTO regusers (first_name,last_name,email,password_hash,created_at,updated_at) VALUES (%(first_name)s,%(last_name)s,%(email)s,%(password)s,%(created_at)s,%(updated_at)s);"
+        mysql.query_db(query, data)
+        print(session)
+        flash("You've been sucessfully registered!","sucess")
+        return redirect('/')
+
+@app.route('/login',methods=['POST'])
+def logIn():
+    #Getting info from the forms
+    provided_email = request.form['email']
+    if len(request.form['email']) < 1:
+        flash("Please enter a valid email address","login_error")
+    #Missing email format validation
+    provided_password = request.form['password']
+    if len(request.form['password']) < 1:
+        flash("Please enter a valid password","login_error")
+
+    if '_flashes' in session.keys():
+        return redirect("/")
+    else:
+        #Fetching current emails and passwords
+        current_data = mysql.query_db("SELECT id,email,first_name,password_hash FROM regusers")
+        print(current_data)
+
+        #Checking if email is in the current data
+        for item in current_data:
+            if item['email'] == provided_email:
+                if bcrypt.check_password_hash(item['password_hash'], provided_password):
+                    session.clear()
+                    session['logged_in'] = True
+                    session['user_id'] = item['id']
+                    session['email'] = item['email']
+                    session['name'] = item['first_name']
+                    print(session)
+                    return redirect('/')
+            
+        flash("Issues with email or password","login_error")
+        return redirect('/')
+
+@app.route('/logout')
+def sessionLogout():
+    session['logged_in'] = False
+    print(session)
+    flash("You've been loged out!","logout")
+    return redirect('/')
+
+@app.route('/send_message',methods=['POST'])
+def sendMessage():
+    data = {
+            'receiver_id': request.form['receiver_id'],
+            'message': request.form['message'],
+            'user_id': session['user_id']
+        }
+    send_query = "INSERT INTO messages (message,sender_id,receiver_id,created_at,updated_at) VALUES (%(message)s,%(user_id)s,%(receiver_id)s,now(),now());"
+    mysql.query_db(send_query,data)
+    return redirect('/')
+
+@app.route('/delete_message/<message_id>')
+def deleteMessage(message_id):
+    #Getting specific message to be deleted and its attributes
+    data = {
+        'message_id': message_id,
+        }
+    query = "SELECT receiver_id FROM messages WHERE id LIKE %(message_id)s;"
+    r_msg_to_be_deleted = mysql.query_db(query,data)[0]['receiver_id']
+    if r_msg_to_be_deleted == session['user_id']:
+        print("match user id and receiver id. about to delete message")
+        delete_data = {
+        'message_id': message_id,
+        }
+        delete_query = "DELETE FROM messages WHERE id LIKE %(message_id)s;"
+        mysql.query_db(delete_query,delete_data)
+        return redirect('/')
+    else:
+        return "you're not allowed to delete this message"
+
+@app.route('/clear_session')
+def clear_session():
+    session.clear()
+    return redirect('/')
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ sqlqueries.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE wall_users;
+
+/* Creating a user table */
+
+CREATE TABLE regusers (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  first_name char(35) NOT NULL DEFAULT '',
+  last_name char(35) NOT NULL DEFAULT '',
+  email char(255) NOT NULL DEFAULT '',
+  password_hash char(255) NOT NULL,
+  PRIMARY KEY (id),
+  created_at datetime,
+  updated_at datetime
+) ENGINE=InnoDB AUTO_INCREMENT=4080 DEFAULT CHARSET=latin1;
+
+/* Inserting users into table */
+
+INSERT INTO regusers (first_name,last_name,email,password_hash,created_at,updated_at)
+VALUES ("Amanda","Demetrio","amandademetrio@gmail.com","password_hashed",now(),now());
+
+/* Creating messages table */
+
+CREATE TABLE messages (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  message char(255) NOT NULL DEFAULT '',
+  sender_id int NOT NULL,
+  receiver_id int NOT NULL,
+  PRIMARY KEY (id),
+  created_at datetime,
+  updated_at datetime
+) ENGINE=InnoDB AUTO_INCREMENT=4080 DEFAULT CHARSET=latin1;
+
+/* Inserting values into messages */
+
+INSERT INTO messages (message,sender_id,receiver_id,created_at,updated_at)
+VALUES 
+("Oi Amanda. Tudo bem por ai?",4081,4080,now(),now()),
+("Hey baby! How are you doing today?",4082,4080,now(),now()),
+("Hey doll! Are you dolling it up today?",4084,4080,now(),now());
